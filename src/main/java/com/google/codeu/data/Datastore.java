5aa421ae09face44e1181247fc1b68fc73ed0ee7
Trisha Gupta <tgupta@tgupta-4whv2t.local>
2019-05-30 12:21:17 +0530

    Steps 3,4 and 5 of Community Page

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -17,6 +17,7 @@
 package com.google.codeu.data;
 
 import com.google.appengine.api.datastore.DatastoreService;
+import com.google.appengine.api.datastore.FetchOptions;
 import com.google.appengine.api.datastore.DatastoreServiceFactory;
 import com.google.appengine.api.datastore.Entity;
 import com.google.appengine.api.datastore.PreparedQuery;
@@ -24,7 +25,9 @@ import com.google.appengine.api.datastore.Query;
 import com.google.appengine.api.datastore.Query.FilterOperator;
 import com.google.appengine.api.datastore.Query.SortDirection;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 /** Provides access to the data stored in Datastore. */
@@ -78,5 +81,22 @@ public class Datastore {
     }
 
     return messages;
+  }
+  public Set<String> getUsers(){
+
+    Set<String> users = new HashSet<>();
+
+    Query query = new Query("Message");
+
+    PreparedQuery results = datastore.prepare(query);
+
+    for(Entity entity : results.asIterable()) {
+
+      users.add((String) entity.getProperty("user"));
+
+    }
+
+    return users;
+
   }
 }
